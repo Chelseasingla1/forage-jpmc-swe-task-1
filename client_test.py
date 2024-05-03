98ba416a209449e8a7253d3698445755bb43a982
Chelsea <chelseasingla123@gmail.com>
2024-05-03 11:49:58 +0530

    new changes in client test file

--- client_test.py
+++ client_test.py
@@ -7,15 +7,26 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    expected_data_points = [
+      ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2),
+      ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)
+    ]
     """ ------------ Add the assertion below ------------ """
+    for quote, expected_data_point in zip(quotes, expected_data_points):
+      self.assertEqual(getDataPoint(quote), expected_data_point)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    expected_data_points = [
+      ('ABC', 120.48, 119.2, (120.48 + 119.2) / 2),
+      ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)
+    ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote, expected_data_point in zip(quotes, expected_data_points):
+      self.assertEqual(getDataPoint(quote), expected_data_point)
 
   """ ------------ Add more unit tests ------------ """
 
